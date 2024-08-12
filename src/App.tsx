970b3c843c30a07dbbd7d5a5d6f2e19dbd55230f
Dulon <dulonmolla88@gmail.com>
2024-08-12 16:09:01 +0600

    test commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+//logo import demo:
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+       
+       <h2 className='text-3xl'>there is new tailwind property component</h2>
+      <h1 className="text-6xl font-bold underline bg-orange-700" >Vite + React</h1>
+      <Button>click me</Button>
+     
+     </div>
+    </>
+  )
+}
+
+export default App
